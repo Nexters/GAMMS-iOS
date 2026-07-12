772a1b80093064bf6963f3857e7004425583e7f9
dlrjswns <leekyunjun@gmail.com>
2026-07-12 22:04:26 +0900

    [INIT] 프로젝트 초기 생성

---
+++ GAMSS/GAMSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GAMSSApp.swift
+//  GAMSS
+//
+//  Created by 이건준 on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct GAMSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
