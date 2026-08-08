16ff55b5430a3b97c470c2fd83a64fcaa9c5296c
dlrjswns <leekyunjun@gmail.com>
2026-08-08 18:49:39 +0900

    [FEAT] Member관련 데이터 모델 추가

---
+++ GAMSS/Sources/Data/DTO/Member/Request/UpdateNicknameRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  UpdateNicknameRequestDTO.swift
+//  GAMSS
+//
+//  Created by 이건준 on 8/8/26.
+//
+
+import Foundation
+
+struct UpdateNicknameRequestDTO: Encodable {
+    let nickname: String
+}
