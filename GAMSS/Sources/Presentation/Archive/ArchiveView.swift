a0d4adb34f5b0a304f3283f08da327f0b9016756
cchanmi <cchanmi518@gmail.com>
2026-08-11 00:00:51 +0900

    [FEAT] ArchiveView placeholder 추가

---
+++ GAMSS/Sources/Presentation/Archive/ArchiveView.swift
@@ -0,0 +1,24 @@
+//
+//  ArchiveView.swift
+//  GAMSS
+//
+//  Created by cchanmi on 8/10/26.
+//
+
+import SwiftUI
+
+struct ArchiveView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("준비 중입니다")
+                .typography(.body3Regular)
+                .foregroundStyle(Color.colorGray500)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ArchiveView()
+}
