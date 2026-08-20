8d6f0042c1034b23b5f25a3ae32b6733403ec413
dlrjswns <leekyunjun@gmail.com>
2026-08-20 13:05:54 +0900

    [ADD] 카드일괄삭제관련 API 응답모델 추가

---
+++ GAMSS/Sources/Data/DTO/Card/Response/DeleteAllCardResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  DeleteAllCardResponseDTO.swift
+//  GAMSS
+//
+//  Created by 이건준 on 8/20/26.
+//
+
+import Foundation
+
+struct DeleteAllCardResponseDTO: Decodable {
+    let deletedCount: Int
+}
